64aba789c4cd80d4897ff82723e924194d7c715c
laurendimitra <lauren.mooore@bluetruckstudios.co>
2018-02-13 17:09:05 -0500

    ughhh

--- public/app.js
+++ public/app.js
@@ -1,11 +1,15 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
+  $.getJSON("/articles", function(data) {
     // For each one
+    console.log(data)
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
+      
       $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].summary + "</p>");
     }
-  });
+  }); 
+
+
   // Whenever someone clicks a p tag
   $(document).on("click", "p", function() {
     // Empty the notes from the note section
--- server.js
+++ server.js
@@ -19,15 +19,44 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/nytscrapeDB", {
   useMongoClient: true
 });
-// Routes
+// Routes 
+
+const scarpedArticlesObject = {
+  scrappedArticles: []
+};
+
+app.get("/", function(req, res) {
+  res.redirect("/articles");
+});
+//GET homepage
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client 
+      res.render("index", scarpedArticlesObject);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
 // A GET route for scraping the echojs website
 app.get("/scrape", function(req, res) {
+  scarpedArticlesObject.scrappedArticles = [];
   // First, we grab the body of the html with request
   axios.get("https://www.nytimes.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
@@ -48,30 +77,16 @@ app.get("/scrape", function(req, res) {
         .then(function(dbArticle) {
           // View the added result in the console
           console.log(dbArticle);
-
+          res.send("it's done")
         })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
-        });
+        // .catch(function(err) {
+        //   // If an error occurred, send it to the client
+        //   return res.json(err);
+        // });
     });
     // If we were able to successfully scrape and save an Article, send a message to the client
-    alert("Scrape Complete");
-
+    res.redirect("/");
   });
-});
-// Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
 });
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
